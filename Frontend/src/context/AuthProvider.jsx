02aad2f724ec36baa17dcf96abc448a2a17e6121
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-02-19 16:37:01 -0500

    creación y actualización de pacientes

--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -1,11 +1,13 @@
 import axios from "axios"
 import { createContext, useEffect, useState } from "react"
-
+//CREACIÓN DEL GRUPO DE WHATSAPP (AUTHCONTEXT)
 const AuthContext = createContext()
-
+//CONTENIDO DLE MENSAJE A SER ENVIADO
+                        //componentes - estudiantes
 const AuthProvider = ({ children }) => {
+    //creación del usestate para el perfil del usuario que va a iniciar sesión
     const [auth, setAuth] = useState({})
-
+//CREAR LA FUNCIÓN OBTENER EL USUARIO DEL ENDPOINT PARA EL PERFIL DEL USUARIO
     const perfil = async(token) => {
         try {
             const url = `${import.meta.env.VITE_BACKEND_URL}/perfil`
@@ -21,6 +23,8 @@ const AuthProvider = ({ children }) => {
             console.log(error);
         }
     }
+    //CREACIÓN DEL useffect para uq se ejecute la función perfil
+    //cuando el componente se cargue
     useEffect(() => {
         const token = localStorage.getItem('token')
         if(token)
@@ -28,9 +32,10 @@ const AuthProvider = ({ children }) => {
             perfil(token)
         }
     }, [])
-    
+    //RETORNA EN EL CONTENIDO DEL MENSAJE
     return (
         <AuthContext.Provider value={
+            //el mensaje a ser enviado
             {
                 auth,
                 setAuth              
